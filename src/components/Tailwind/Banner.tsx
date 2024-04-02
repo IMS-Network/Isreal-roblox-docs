23f3d9e0d76a67679a39aecd131918ff3934c9e7
Aviv zusim <avivzusim@gmail.com>
2024-04-02 06:18:41 +0300

    + Bug fix

--- src/components/Tailwind/Banner.tsx
+++ src/components/Tailwind/Banner.tsx
@@ -2,12 +2,6 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
 
 export default function HeroBanner() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
